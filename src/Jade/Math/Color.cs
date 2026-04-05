c48d810694cb41338b9acc784759362499da6ca4
Aerafal <jimmy.mll@hotmail.com>
2026-04-05 18:49:08 +0200

    feat: add Color and Color8 structs for color representation

---
+++ src/Jade/Math/Color.cs
@@ -0,0 +1,67 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.Runtime.InteropServices;
+
+namespace Jade.Math;
+
+[StructLayout(LayoutKind.Sequential)]
+public readonly struct Color :
+    IFormattable,
+    IEquatable<Color>
+{
+    public readonly float R;
+
+    public readonly float G;
+
+    public readonly float B;
+
+    public readonly float A;
+
+    public Color(float r, float g, float b, float a)
+    {
+        R = r;
+        G = g;
+        B = b;
+        A = a;
+    }
+
+    public bool Equals(Color other)
+    {
+        return R.Equals(other.R) && G.Equals(other.G) && B.Equals(other.B) && A.Equals(other.A);
+    }
+
+    public override bool Equals(object? obj)
+    {
+        return obj is Color other && Equals(other);
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(R, G, B, A);
+    }
+
+    public string ToString([StringSyntax(StringSyntaxAttribute.NumericFormat)] string? format, IFormatProvider? formatProvider)
+    {
+        return $"Color(R={R.ToString(format, formatProvider)}, G={G.ToString(format, formatProvider)}, B={B.ToString(format, formatProvider)}, A={A.ToString(format, formatProvider)}";
+    }
+
+    public string ToString([StringSyntax(StringSyntaxAttribute.NumericFormat)] string? format)
+    {
+        return ToString(format, CultureInfo.CurrentCulture);
+    }
+
+    public override string ToString()
+    {
+        return ToString("G", CultureInfo.CurrentCulture);
+    }
+
+    public static bool operator ==(Color left, Color right)
+    {
+        return left.Equals(right);
+    }
+
+    public static bool operator !=(Color left, Color right)
+    {
+        return !left.Equals(right);
+    }
+}
---
+++ src/Jade/Math/Color8.cs
@@ -0,0 +1,67 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.Runtime.InteropServices;
+
+namespace Jade.Math;
+
+[StructLayout(LayoutKind.Sequential)]
+public readonly struct Color8 :
+    IFormattable,
+    IEquatable<Color8>
+{
+    public readonly byte R;
+
+    public readonly byte G;
+
+    public readonly byte B;
+
+    public readonly byte A;
+
+    public Color8(byte r, byte g, byte b, byte a)
+    {
+        R = r;
+        G = g;
+        B = b;
+        A = a;
+    }
+
+    public bool Equals(Color8 other)
+    {
+        return R == other.R && G == other.G && B == other.B && A == other.A;
+    }
+
+    public override bool Equals(object? obj)
+    {
+        return obj is Color8 other && Equals(other);
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(R, G, B, A);
+    }
+
+    public string ToString([StringSyntax(StringSyntaxAttribute.NumericFormat)] string? format, IFormatProvider? formatProvider)
+    {
+        return $"Color8(R={R.ToString(format, formatProvider)}, G={G.ToString(format, formatProvider)}, B={B.ToString(format, formatProvider)}, A={A.ToString(format, formatProvider)}";
+    }
+
+    public string ToString([StringSyntax(StringSyntaxAttribute.NumericFormat)] string? format)
+    {
+        return ToString(format, CultureInfo.CurrentCulture);
+    }
+
+    public override string ToString()
+    {
+        return ToString("G", CultureInfo.CurrentCulture);
+    }
+
+    public static bool operator ==(Color8 left, Color8 right)
+    {
+        return left.Equals(right);
+    }
+
+    public static bool operator !=(Color8 left, Color8 right)
+    {
+        return !left.Equals(right);
+    }
+}
