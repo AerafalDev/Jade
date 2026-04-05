6405183e399c80184ec51e6388f137f82cee3060
Aerafal <jimmy.mll@hotmail.com>
2026-04-05 21:54:10 +0200

    feat: add Rectangle struct with formatting and equality support

---
+++ src/Jade/Math/Rectangle.cs
@@ -0,0 +1,67 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.Runtime.InteropServices;
+
+namespace Jade.Math;
+
+[StructLayout(LayoutKind.Sequential)]
+public readonly struct Rectangle :
+    IFormattable,
+    IEquatable<Rectangle>
+{
+    public readonly int X;
+
+    public readonly int Y;
+
+    public readonly int Width;
+
+    public readonly int Height;
+
+    public Rectangle(int x, int y, int width, int height)
+    {
+        X = x;
+        Y = y;
+        Width = width;
+        Height = height;
+    }
+
+    public bool Equals(Rectangle other)
+    {
+        return X == other.X && Y == other.Y && Width == other.Width && Height == other.Height;
+    }
+
+    public override bool Equals(object? obj)
+    {
+        return obj is Rectangle other && Equals(other);
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(X, Y, Width, Height);
+    }
+
+    public string ToString([StringSyntax(StringSyntaxAttribute.NumericFormat)] string? format, IFormatProvider? formatProvider)
+    {
+        return $"Rectangle(X={X.ToString(format, formatProvider)}, Y={Y.ToString(format, formatProvider)}, Width={Width.ToString(format, formatProvider)}, Height={Height.ToString(format, formatProvider)}";
+    }
+
+    public string ToString([StringSyntax(StringSyntaxAttribute.NumericFormat)] string? format)
+    {
+        return ToString(format, CultureInfo.CurrentCulture);
+    }
+
+    public override string ToString()
+    {
+        return ToString("G", CultureInfo.CurrentCulture);
+    }
+
+    public static bool operator ==(Rectangle left, Rectangle right)
+    {
+        return left.Equals(right);
+    }
+
+    public static bool operator !=(Rectangle left, Rectangle right)
+    {
+        return !left.Equals(right);
+    }
+}
